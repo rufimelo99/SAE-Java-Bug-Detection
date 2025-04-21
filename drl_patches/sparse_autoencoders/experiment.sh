48caee8bbc0e3b86c92a2066e82a618e0472618c
Rui Melo <rufimelo99@gmail.com>
2025-04-21 19:56:57 +0100

    Update experiment.sh

--- drl_patches/sparse_autoencoders/experiment.sh
+++ drl_patches/sparse_autoencoders/experiment.sh
@@ -55,6 +55,57 @@ echo "Running pipeline for humaneval"
 run_pipeline "humaneval" "artifacts/humaneval.csv" "artifacts/humaneval_train_indexes.json"
 
 
+run_gemma2b_pipeline() {
+    dataset=$1
+    csv_path=$2
+    train_indexes=$3
+
+    echo "Running pipeline for $dataset from $csv_path with train indexes $train_indexes"
+
+    for i in {0..24}; do
+        output_dir=$BASE_DIR/gemma2_${dataset}/layer$i
+        
+        python sae_exploration.py \
+            --csv_path $csv_path \
+            --layer $i \
+            --model google/gemma-2-2b \
+            --release gemma-scope-2b-pt-res-canonical \
+            --sae_id layer_$i/width_16k/canonical \
+            --cache_component hook_resid_post.hook_sae_acts_post \
+            --output_dir $BASE_DIR/gbug-java/layer$i
+
+
+        python3 vulnerability_detection_features.py \
+            --dir-path gemma2_${dataset}/layer$i/ \
+            --train-indexes_path $train_indexes \
+            --save-model
+    done
+}
+
+# Run for each dataset
+echo "Running pipeline for gbug-java"
+run_gemma2b_pipeline "gbug-java" "artifacts/gbug-java.csv" "artifacts/gbug-java_train_indexes.json"
+echo "Running pipeline for defects4j"
+run_gemma2b_pipeline "defects4j" "artifacts/defects4j.csv" "artifacts/defects4j_train_indexes.json"
+echo "Running pipeline for humaneval"
+run_gemma2b_pipeline "humaneval" "artifacts/humaneval.csv" "artifacts/humaneval_train_indexes.json"
+
+
+# for i in {1..33};
+# do
+#   python3 sae_exploration.py \
+#     --csv_path $BASE_DIR/artifacts/defects4j.csv \
+#     --layer $i \
+#     --model meta-llama/Llama-3.1-8B \
+#     --release llama_scope_lxr_32x \
+#     --sae_id l${i}r_32x \
+#     --cache_component hook_resid_post.hook_sae_acts_post \
+#     --output_dir $BASE_DIR/llama_defects4j/layer$i
+# done
+
+
+
+
 run_bert_pipeline() {
     model=$1
 
@@ -83,10 +134,51 @@ run_bert_pipeline "answerdotai/ModernBERT-large"
 
 
 
+run_hidden_states_pipeline() {
+    model=$1
+    csv_path=$2
+    output_dir=$3
 
-python baselines/gather_hidden_states.py --csv_path artifacts/gbug-java.csv --output_dir gemma2_hidden_states_gbug-java --model_name google/gemma-2-2b
-python baselines/gather_hidden_states.py --csv_path artifacts/defects4j.csv --output_dir gemma2_hidden_states_defects --model_name google/gemma-2-2b
-python baselines/gather_hidden_states.py --csv_path artifacts/humaneval.csv --output_dir gemma2_hidden_states_humaneval --model_name google/gemma-2-2b
+    echo "Running pipeline for $model, $csv_path, $output_dir"
+
+    python baselines/gather_hidden_states.py \
+        --csv_path $csv_path \
+        --output_dir $output_dir \
+        --model_name $model
+
+}
+
+# Run for each dataset
+run_hidden_states_pipeline openai-community/gpt2 artifacts/gbug-java.csv gpt2_hidden_states_gbug-java
+run_hidden_states_pipeline openai-community/gpt2 artifacts/defects4j.csv gpt2_hidden_states_defects
+run_hidden_states_pipeline openai-community/gpt2 artifacts/humaneval.csv gpt2_hidden_states_humaneval
+
+run_hidden_states_pipeline google/gemma-2-2b artifacts/gbug-java.csv gemma2_hidden_states_gbug-java
+run_hidden_states_pipeline google/gemma-2-2b artifacts/defects4j.csv gemma2_hidden_states_defects
+run_hidden_states_pipeline google/gemma-2-2b artifacts/humaneval.csv gemma2_hidden_states_humaneval
+
+
+run_gpt2_hidden_states_prediction_pipeline() {
+    train_indexes=$1
+    dataset=$2
+
+    echo "Running gpt2 hidden states pipeline for $dataset, with $train_indexes"
+
+    for i in {0..11}; do
+        output_dir=$BASE_DIR/gpt2_${dataset}/layer$i
+        
+        python3 vulnerability_detection_features.py \
+                --dir-path gpt2_hidden_states_${dataset}/layer$i/ \
+                --train-indexes_path $train_indexes \
+                --save-model
+    done
+}
+
+run_gpt2_hidden_states_prediction_pipeline "artifacts/gbug-java_train_indexes.json" "gbug-java"
+run_gpt2_hidden_states_prediction_pipeline "artifacts/defects4j_train_indexes.json" "defects4j"
+run_gpt2_hidden_states_prediction_pipeline "artifacts/humaneval_train_indexes.json" "humaneval"
+
+
 
 
 # Baselines
